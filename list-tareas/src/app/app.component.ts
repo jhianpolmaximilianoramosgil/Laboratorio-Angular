6264617b3c6fa2f9b7d0110bbe24fdd8769d2f02
Jhianpol <jhianpol.ramos@vallegrande.edu.pe>
2021-12-12 01:17:32 -0500

    Mi nueva App Listado de Tareas

---
+++ list-tareas/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ListTareas';
+  myimage:string = "assets/images/bienvenidos.gif";
+}
